971771125dfcd2d74202e216b5cce568e429d972
programador2 <paula.wmarch@alumnos.upm.es>
2024-04-16 18:25:13 +0200

    Reduce the number of non-empty switch cases from 7 to at most 4.  #1

--- src/main/java/com/practica/ems/covid/ContactosCovid.java
+++ src/main/java/com/practica/ems/covid/ContactosCovid.java
@@ -238,34 +238,20 @@ public class ContactosCovid {
 
 	private Persona crearPersona(String[] data) {
 		Persona persona = new Persona();
-		for (int i = 1; i < Constantes.MAX_DATOS_PERSONA; i++) {
-			String s = data[i];
-			switch (i) {
-			case 1:
-				persona.setDocumento(s);
-				break;
-			case 2:
-				persona.setNombre(s);
-				break;
-			case 3:
-				persona.setApellidos(s);
-				break;
-			case 4:
-				persona.setEmail(s);
-				break;
-			case 5:
-				persona.setDireccion(s);
-				break;
-			case 6:
-				persona.setCp(s);
-				break;
-			case 7:
-				persona.setFechaNacimiento(FechaHora.parsearFecha(s));
-				break;
-			}
+		// Asegúrate de que haya suficientes elementos en el arreglo data
+		if (data.length >= Constantes.MAX_DATOS_PERSONA) {
+			// Asigna directamente los valores a los campos de Persona
+			persona.setDocumento(data[1]);
+			persona.setNombre(data[2]);
+			persona.setApellidos(data[3]);
+			persona.setEmail(data[4]);
+			persona.setDireccion(data[5]);
+			persona.setCp(data[6]);
+			persona.setFechaNacimiento(FechaHora.parsearFecha(data[7]));
 		}
 		return persona;
 	}
+
 
 	private PosicionPersona crearPosicionPersona(String[] data) {
 		PosicionPersona posicionPersona = new PosicionPersona();
